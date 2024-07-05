eb2c68198c178a8a228763f3e9ee46f41e4be1b6
Seth Spearman <sethspearman@gmail.com>
2024-07-05 16:28:49 -0400

    added maui components  Changes to be committed: 	new file:   CocktailChooser.Maui/App.xaml 	new file:   CocktailChooser.Maui/App.xaml.cs 	new file:   CocktailChooser.Maui/CocktailChooser.Maui.csproj 	new file:   CocktailChooser.Maui/Components/Layout/MainLayout.razor 	new file:   CocktailChooser.Maui/Components/Layout/MainLayout.razor.css 	new file:   CocktailChooser.Maui/Components/Layout/NavMenu.razor 	new file:   CocktailChooser.Maui/Components/Layout/NavMenu.razor.css 	new file:   CocktailChooser.Maui/Components/Pages/Counter.razor 	new file:   CocktailChooser.Maui/Components/Pages/Home.razor 	new file:   CocktailChooser.Maui/Components/Pages/Weather.razor 	new file:   CocktailChooser.Maui/Components/Routes.razor 	new file:   CocktailChooser.Maui/Components/_Imports.razor 	new file:   CocktailChooser.Maui/MainPage.xaml 	new file:   CocktailChooser.Maui/MainPage.xaml.cs 	new file:   CocktailChooser.Maui/MauiProgram.cs 	new file:   CocktailChooser.Maui/Platforms/Android/AndroidManifest.xml 	new file:   CocktailChooser.Maui/Platforms/Android/MainActivity.cs 	new file:   CocktailChooser.Maui/Platforms/Android/MainApplication.cs 	new file:   CocktailChooser.Maui/Platforms/Android/Resources/values/colors.xml 	new file:   CocktailChooser.Maui/Platforms/MacCatalyst/AppDelegate.cs 	new file:   CocktailChooser.Maui/Platforms/MacCatalyst/Entitlements.plist 	new file:   CocktailChooser.Maui/Platforms/MacCatalyst/Info.plist 	new file:   CocktailChooser.Maui/Platforms/MacCatalyst/Program.cs 	new file:   CocktailChooser.Maui/Platforms/Tizen/Main.cs 	new file:   CocktailChooser.Maui/Platforms/Tizen/tizen-manifest.xml 	new file:   CocktailChooser.Maui/Platforms/Windows/App.xaml 	new file:   CocktailChooser.Maui/Platforms/Windows/App.xaml.cs 	new file:   CocktailChooser.Maui/Platforms/Windows/Package.appxmanifest 	new file:   CocktailChooser.Maui/Platforms/Windows/app.manifest 	new file:   CocktailChooser.Maui/Platforms/iOS/AppDelegate.cs 	new file:   CocktailChooser.Maui/Platforms/iOS/Info.plist 	new file:   CocktailChooser.Maui/Platforms/iOS/Program.cs 	new file:   CocktailChooser.Maui/Properties/launchSettings.json 	new file:   CocktailChooser.Maui/Resources/AppIcon/appicon.svg 	new file:   CocktailChooser.Maui/Resources/AppIcon/appiconfg.svg 	new file:   CocktailChooser.Maui/Resources/Fonts/OpenSans-Regular.ttf 	new file:   CocktailChooser.Maui/Resources/Images/dotnet_bot.svg 	new file:   CocktailChooser.Maui/Resources/Raw/AboutAssets.txt 	new file:   CocktailChooser.Maui/Resources/Splash/splash.svg 	new file:   CocktailChooser.Maui/wwwroot/css/app.css 	new file:   CocktailChooser.Maui/wwwroot/css/bootstrap/bootstrap.min.css 	new file:   CocktailChooser.Maui/wwwroot/css/bootstrap/bootstrap.min.css.map 	new file:   CocktailChooser.Maui/wwwroot/favicon.png 	new file:   CocktailChooser.Maui/wwwroot/index.html 	modified:   CocktailChooser.sln

---
+++ CocktailChooser.Maui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CocktailChooser.Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
